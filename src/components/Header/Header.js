141ecdb812e96a022c0921c7a969efb8355f98be
marcelo-magalhaes <magalhaessmarcelos@gmail.com>
2024-01-15 14:57:50 -0300

    first commit/feat: Header and Wrapper criation

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+
+import './Header.css';
+
+
+export default function Header(){
+    return (
+    <div className='header'>
+        <span>My Link</span>
+        <button> Sair </button>
+    </div>
+    )
+}
